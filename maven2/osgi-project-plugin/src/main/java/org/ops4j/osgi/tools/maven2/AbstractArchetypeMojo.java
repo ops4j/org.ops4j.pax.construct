8fe0f95ecd765f27ea5211efbaec494264672b1d
Stuart McCulloch <mcculls@gmail.com>
2007-03-19 09:33:05 +0000

    Add plugin goal to create new OSGi project.
    git-svn-id: https://scm.ops4j.org/repos/ops4j/laboratory/users/stuart/tools@5852 9b982a3c-3ae5-0310-a4bc-d9a3335569bd

---
+++ maven2/osgi-project-plugin/src/main/java/org/ops4j/osgi/tools/maven2/AbstractArchetypeMojo.java
@@ -0,0 +1,83 @@
+package org.ops4j.osgi.tools.maven2;
+
+/*
+ * Copyright 2007 Stuart McCulloch
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.codehaus.plexus.util.cli.CommandLineException;
+import org.codehaus.plexus.util.cli.CommandLineUtils;
+import org.codehaus.plexus.util.cli.Commandline;
+import org.codehaus.plexus.util.cli.StreamConsumer;
+
+import java.io.File;
+
+/**
+ * Foundation for all OSGi project goals that use archetypes.
+ */
+public abstract class AbstractArchetypeMojo
+    extends AbstractMojo
+{
+    /**
+     * @parameter expression="${maven.home}/bin/mvn"
+     * @required
+     */
+    private File mvn;
+
+    private final static String archetypeGroupId    = "org.ops4j.osgi.tools.maven2";
+    private final static String archetypeArtifactId = "osgi-project-archetype";
+    private final static String archetypeVersion    = "0.1.0-SNAPSHOT"; // TODO: use RELEASE/LATEST when deployed?
+
+    public void execute()
+        throws MojoExecutionException
+    {
+        Commandline commandLine = new Commandline();
+
+        commandLine.setExecutable( mvn.getAbsolutePath() );
+
+        commandLine.createArgument().setValue( "archetype:create" );
+        commandLine.createArgument().setValue( "-DarchetypeGroupId="+archetypeGroupId );
+        commandLine.createArgument().setValue( "-DarchetypeArtifactId="+archetypeArtifactId );
+        commandLine.createArgument().setValue( "-DarchetypeVersion="+archetypeVersion );
+
+        createSubArguments( commandLine );
+
+        StreamConsumer consumer = new StreamConsumer()
+        {
+            public void consumeLine( String line )
+            {
+                getLog().info( line );
+            }
+        };
+
+        try
+        {
+            int result = CommandLineUtils.executeCommandLine( commandLine, null, consumer );
+
+            if ( result != 0 )
+            {
+                throw new MojoExecutionException( "Result of " + commandLine + " execution is: '" + result + "'." );
+            }
+        }
+        catch ( CommandLineException e )
+        {
+            throw new MojoExecutionException( "Command execution failed.", e );
+        }
+    }
+
+    abstract protected void createSubArguments( Commandline commandLine );
+}
+
---
+++ maven2/osgi-project-plugin/src/main/java/org/ops4j/osgi/tools/maven2/CreateMojo.java
@@ -0,0 +1,53 @@
+package org.ops4j.osgi.tools.maven2;
+
+/*
+ * Copyright 2007 Stuart McCulloch
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import org.codehaus.plexus.util.cli.Commandline;
+
+/**
+ * Goal which creates a new OSGi project.
+ *
+ * @requiresProject false
+ *
+ * @goal create
+ */
+public class CreateMojo
+    extends AbstractArchetypeMojo
+{
+    /**
+     * The groupId of the OSGi project to create.
+     * 
+     * @parameter expression="${groupId}"
+     * @required
+     */
+    private String groupId;
+
+    /**
+     * The artifactId of the OSGi project to create.
+     * 
+     * @parameter expression="${artifactId}"
+     * @required
+     */
+    private String artifactId;
+
+    protected void createSubArguments( Commandline commandLine )
+    {
+        commandLine.createArgument().setValue( "-DgroupId="+groupId+"."+artifactId );
+        commandLine.createArgument().setValue( "-DartifactId="+artifactId.replace('.','-') );
+    }
+}
+
