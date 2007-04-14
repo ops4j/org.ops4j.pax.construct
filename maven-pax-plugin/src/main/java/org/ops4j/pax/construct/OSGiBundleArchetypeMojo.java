c9b8c39c19850a90b9036b709dc1ce308ac65372
Stuart McCulloch <mcculls@gmail.com>
2007-04-14 16:19:39 +0000

    Fix export packages when the template interface and activator are in separate bundles.
    git-svn-id: https://scm.ops4j.org/repos/ops4j/projects/pax/construct@6108 9b982a3c-3ae5-0310-a4bc-d9a3335569bd

--- maven-pax-plugin/src/main/java/org/ops4j/pax/construct/OSGiBundleArchetypeMojo.java
+++ maven-pax-plugin/src/main/java/org/ops4j/pax/construct/OSGiBundleArchetypeMojo.java
@@ -17,11 +17,14 @@ package org.ops4j.pax.construct;
  */
 
 import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
 
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.shared.model.fileset.FileSet;
 import org.apache.maven.shared.model.fileset.util.FileSetManager;
+
+import org.codehaus.plexus.util.IOUtil;
 
 /**
  * Create a new skeleton bundle and add it to an existing OSGi project.
@@ -87,16 +90,18 @@ public final class OSGiBundleArchetypeMojo extends AbstractArchetypeMojo
         FileSet activatorFiles = new FileSet();
         activatorFiles.setDirectory( project.getBasedir() + File.separator + bundleName );
 
-        if ( provideInterface == false )
+        if ( !provideInterface )
         {
             activatorFiles.addInclude( "src/main/java/**/ExampleService.java" );
         }
 
-        if ( provideActivator == false )
+        if ( !provideActivator )
         {
             activatorFiles.addInclude( "src/main/java/**/internal" );
             activatorFiles.addInclude( "src/main/resources" );
         }
+
+        FileWriter out = null;
 
         try
         {
@@ -104,10 +109,28 @@ public final class OSGiBundleArchetypeMojo extends AbstractArchetypeMojo
             {
                 new FileSetManager( getLog(), false ).delete( activatorFiles );
             }
+
+            if ( provideActivator && !provideInterface )
+            {
+                /*
+                 * Interface x.Y will be in another bundle, so null the export packages (default is export package x)
+                 *
+                 * If we don't do this then BND will automatically add interface Y to our generated bundle, as without
+                 * interface Y we'd only be exporting part of package x, which is bad as it leads to a split package.
+                 *
+                 * We only want interface Y in one bundle (the other one), so we reset our Export-Package setting...
+                 */
+                out = new FileWriter( activatorFiles.getDirectory() + "/src/main/resources/META-INF/details.bnd", true );
+                out.write( "Export-Package:" + System.getProperty( "line.separator" ) );
+            }
         }
         catch ( IOException e )
         {
-            throw new MojoExecutionException( "I/O error while deleting files", e );
+            throw new MojoExecutionException( "I/O error while patching files", e );
+        }
+        finally
+        {
+            IOUtil.close( out );
         }
     }
 }
