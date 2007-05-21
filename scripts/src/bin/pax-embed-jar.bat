1efe9c39cda0fdb5ef11d201535a4c2b690d9b49
Stuart McCulloch <mcculls@gmail.com>
2007-05-21 06:26:28 +0000

    PAXCONSTRUCT-7: scripts to simplify embedding of jars inside OSGi bundles
    git-svn-id: https://scm.ops4j.org/repos/ops4j/projects/pax/construct@6556 9b982a3c-3ae5-0310-a4bc-d9a3335569bd

---
+++ scripts/src/bin/pax-embed-jar.bat
@@ -0,0 +1,49 @@
+@echo off
+SETLOCAL
+set _SCRIPTS_=%~dp0
+call "%_SCRIPTS_%\pax-validate"
+
+set _BATFILE_=%0
+set _GROUPID_=
+set _ARTIFACTID_=
+set _VERSION_=
+
+goto getopts
+:shift_2
+shift
+:shift_1
+shift
+
+:getopts
+if "%1"=="-g" set _GROUPID_=%2
+if "%1"=="-g" goto shift_2
+if "%1"=="-a" set _ARTIFACTID_=%2
+if "%1"=="-a" goto shift_2
+if "%1"=="-v" set _VERSION_=%2
+if "%1"=="-v" goto shift_2
+if "%1"=="-h" goto help
+if "%1"=="--" goto endopts
+if "%1"=="" goto endopts
+
+echo %_BATFILE_%: illegal option -- %1
+:help
+echo pax-embed-jar -g groupId -a artifactId -v version [-- mvnopts ...]
+goto done
+:endopts
+
+shift
+shift
+
+set _EXTRA_=%PAX_CONSTRUCT_OPTIONS% %0 %1 %2 %3 %4 %5 %6 %7 %8 %9
+
+if ""=="%_GROUPID_%" set /p _GROUPID_="library groupId (org.ops4j.example) ? "
+if ""=="%_ARTIFACTID_%" set /p _ARTIFACTID_="library artifactId (myJarFile) ? "
+if ""=="%_VERSION_%" set /p _VERSION_="library version (0.1.0-SNAPSHOT) ? "
+
+if ""=="%_GROUPID_%" set _GROUPID_=org.ops4j.example
+if ""=="%_ARTIFACTID_%" set _ARTIFACTID_=myJarFile
+if ""=="%_VERSION_%" set _VERSION_=0.1.0-SNAPSHOT
+
+@echo on
+mvn pax:embed-jar -DgroupId=%_GROUPID_% -DartifactId=%_ARTIFACTID_% -Dversion=%_VERSION_% %_EXTRA_%
+:done
