b56f41295b4710f08902ca242034b6aa39c98aa6
Stuart McCulloch <mcculls@gmail.com>
2007-05-21 09:29:16 +0000

    PAXCONSTRUCT-21: the idea is that the dependency details can be pulled from the relevant pom in the project, discovered using the bundle name
    git-svn-id: https://scm.ops4j.org/repos/ops4j/projects/pax/construct@6561 9b982a3c-3ae5-0310-a4bc-d9a3335569bd

---
+++ scripts/src/bin/pax-use-bundle.bat
@@ -0,0 +1,38 @@
+@echo off
+SETLOCAL
+set _SCRIPTS_=%~dp0
+call "%_SCRIPTS_%\pax-validate"
+
+set _BATFILE_=%0
+set _NAME_=
+
+goto getopts
+:shift_2
+shift
+:shift_1
+shift
+
+:getopts
+if "%1"=="-n" set _NAME_=%2
+if "%1"=="-n" goto shift_2
+if "%1"=="-h" goto help
+if "%1"=="--" goto endopts
+if "%1"=="" goto endopts
+
+echo %_BATFILE_%: illegal option -- %1
+:help
+echo pax-use-bundle -n bundleName [-- mvnOpts ...]
+goto done
+:endopts
+
+shift
+shift
+
+set _EXTRA_=%PAX_CONSTRUCT_OPTIONS% %0 %1 %2 %3 %4 %5 %6 %7 %8 %9
+
+if ""=="%_NAME_%" set /p _NAME_="bundle name (myBundle) ? "
+if ""=="%_NAME_%" set _NAME_=myBundle
+
+@echo on
+mvn pax:use-bundle -Dname=%_NAME_% %_EXTRA_%
+:done
