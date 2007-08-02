18ffa6c3b4349642290403d9e5df36459e8af32c
Stuart McCulloch <mcculls@gmail.com>
2007-08-02 05:23:34 +0000

    PAXCONSTRUCT-33: first draft of move scripts
    git-svn-id: https://scm.ops4j.org/repos/ops4j/projects/pax/construct@7538 9b982a3c-3ae5-0310-a4bc-d9a3335569bd

---
+++ scripts/src/bin/pax-move-bundle.bat
@@ -0,0 +1,44 @@
+@echo off
+SETLOCAL
+set _SCRIPTS_=%~dp0
+call "%_SCRIPTS_%\pax-validate"
+
+set _BATFILE_=%0
+set _NAME_=
+set _DIR_=
+
+goto getopts
+:shift_2
+shift
+:shift_1
+shift
+
+:getopts
+if "%1"=="-n" set _NAME_=%2
+if "%1"=="-n" goto shift_2
+if "%1"=="-d" set _DIR_=%2
+if "%1"=="-d" goto shift_2
+if "%1"=="-h" goto help
+if "%1"=="--" goto endopts
+if "%1"=="" goto endopts
+
+echo %_BATFILE_%: illegal option -- %1
+:help
+echo pax-move-bundle -n bundleName -d directory [-- mvnOpts ...]
+goto done
+:endopts
+
+shift
+shift
+
+set _EXTRA_=%PAX_CONSTRUCT_OPTIONS% %0 %1 %2 %3 %4 %5 %6 %7 %8 %9
+
+if ""=="%_NAME_%" set /p _NAME_="bundle name (myBundle) ? "
+if ""=="%_DIR_%" set /p _DIR_="to directory (.) ? "
+
+if ""=="%_NAME_%" set _NAME_=myBundle
+if ""=="%_DIR_%" set _DIR_=.
+
+@echo on
+mvn pax:move-bundle -Dname=%_NAME_% -Dlocation=%_DIR_% %_EXTRA_%
+:done
