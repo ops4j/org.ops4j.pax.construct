58d8532c802b6a0d6a1ab0936218ae71e48ae371
Stuart McCulloch <mcculls@gmail.com>
2007-09-13 10:35:25 +0000

    cleanup sample scripts folder
    git-svn-id: https://scm.ops4j.org/repos/ops4j/projects/pax/construct@8509 9b982a3c-3ae5-0310-a4bc-d9a3335569bd

---
+++ samples/SPRING_OSGI_EXAMPLE.bat
@@ -0,0 +1,36 @@
+
+call pax-create-project -g examples -a spring-osgi-example
+
+cd spring-osgi-example
+
+call pax-add-repository -i spring-osgi -u http://static.springframework.org/maven2-snapshots
+call pax-add-repository -i spring -u http://s3.amazonaws.com/maven.springframework.org/milestone
+
+set OLD_CONSTRUCT_OPTIONS=%PAX_CONSTRUCT_OPTIONS%
+set PAX_CONSTRUCT_OPTIONS=-DaddVersion -DtargetDirectory=wrappers %OLD_CONSTRUCT_OPTIONS%
+
+call pax-wrap-jar -g backport-util-concurrent -a backport-util-concurrent -v 3.0 -- "-DimportPackage=sun.misc;resolution:=optional,*"
+call pax-wrap-jar -g commons-logging -a commons-logging -v 1.1 -- "-DimportPackage=org.apache.commons.*,*;resolution:=optional"
+call pax-wrap-jar -g commons-collections -a commons-collections -v 3.2
+
+call pax-wrap-jar -g asm -a asm -v 2.2.3
+call pax-wrap-jar -g aopalliance -a aopalliance -v 1.0
+call pax-wrap-jar -g junit -a junit -v 3.8.1
+
+set PAX_CONSTRUCT_OPTIONS=%OLD_CONSTRUCT_OPTIONS%
+
+call pax-import-bundle -g org.springframework -a spring-core -v 2.1-m3
+call pax-import-bundle -g org.springframework -a spring-context -v 2.1-m3
+call pax-import-bundle -g org.springframework -a spring-beans -v 2.1-m3
+
+call pax-import-bundle -g org.springframework.osgi -a spring-aop -v 2.1-osgi-m4-SNAPSHOT
+
+call pax-import-bundle -g org.springframework.osgi -a spring-osgi-io -v 1.0-m3-SNAPSHOT
+call pax-import-bundle -g org.springframework.osgi -a spring-osgi-core -v 1.0-m3-SNAPSHOT
+call pax-import-bundle -g org.springframework.osgi -a spring-osgi-extender -v 1.0-m3-SNAPSHOT
+
+call pax-import-bundle -g org.springframework.osgi.samples -a weather-dao -v 1.0-m3-SNAPSHOT
+call pax-import-bundle -g org.springframework.osgi.samples -a weather-service -v 1.0-m3-SNAPSHOT
+
+call mvn clean install pax:provision
+
