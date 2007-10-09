5a9f0f164c6c0a45ec24507b4538980637b975f1
Stuart McCulloch <mcculls@gmail.com>
2007-10-09 06:17:12 +0000

    Hide error message from user
    git-svn-id: https://scm.ops4j.org/repos/ops4j/projects/pax/construct@8878 9b982a3c-3ae5-0310-a4bc-d9a3335569bd

--- scripts/src/bin/pax-validate.bat
+++ scripts/src/bin/pax-validate.bat
@@ -4,7 +4,7 @@ set _SCRIPTS_=%~dp0
 
 if ""=="%PAX_CONSTRUCT_VERSION%" set PAX_CONSTRUCT_VERSION=${project.version}
 
-call mvn -o -npu -N -f "%_SCRIPTS_%\pax-bootstrap-pom.xml" -DPAX_CONSTRUCT_VERSION=%PAX_CONSTRUCT_VERSION% validate | find "ERROR"
+call mvn -o -npu -N -f "%_SCRIPTS_%\pax-bootstrap-pom.xml" -DPAX_CONSTRUCT_VERSION=%PAX_CONSTRUCT_VERSION% validate | find "ERROR" >NUL
 goto answer%ERRORLEVEL%
 
 :answer1
