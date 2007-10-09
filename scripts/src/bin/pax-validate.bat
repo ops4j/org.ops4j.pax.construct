9094a1ef4451f3e1c88e188f81f84440e82eaaba
Stuart McCulloch <mcculls@gmail.com>
2007-10-09 05:02:41 +0000

    Workaround Windows 2000 ERRORLEVEL bug in Maven 2.0.7
    git-svn-id: https://scm.ops4j.org/repos/ops4j/projects/pax/construct@8876 9b982a3c-3ae5-0310-a4bc-d9a3335569bd

--- scripts/src/bin/pax-validate.bat
+++ scripts/src/bin/pax-validate.bat
@@ -4,14 +4,14 @@ set _SCRIPTS_=%~dp0
 
 if ""=="%PAX_CONSTRUCT_VERSION%" set PAX_CONSTRUCT_VERSION=${project.version}
 
-call mvn -o -npu -N -f "%_SCRIPTS_%\pax-bootstrap-pom.xml" -DPAX_CONSTRUCT_VERSION=%PAX_CONSTRUCT_VERSION% validate
+call mvn -o -npu -N -f "%_SCRIPTS_%\pax-bootstrap-pom.xml" -DPAX_CONSTRUCT_VERSION=%PAX_CONSTRUCT_VERSION% validate | find "ERROR"
 goto answer%ERRORLEVEL%
 
-:answer0
+:answer1
   :: already installed and validated
   goto done
 
-:answer1
+:answer0
   echo BOOTSTRAP PAX-CONSTRUCT PLUGIN
   echo ==============================
   @echo on
