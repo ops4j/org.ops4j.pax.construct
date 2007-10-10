78bee773736f9be42cab542711308083c71a7321
Stuart McCulloch <mcculls@gmail.com>
2007-10-10 08:58:48 +0000

    Update examples to use new behaviour
    git-svn-id: https://scm.ops4j.org/repos/ops4j/projects/pax/construct@8909 9b982a3c-3ae5-0310-a4bc-d9a3335569bd

--- samples/SPRING_OSGI_EXAMPLE.bat
+++ samples/SPRING_OSGI_EXAMPLE.bat
@@ -9,14 +9,14 @@ call pax-add-repository -i spring -u http://s3.amazonaws.com/maven.springframewo
 set OLD_CONSTRUCT_OPTIONS=%PAX_CONSTRUCT_OPTIONS%
 set PAX_CONSTRUCT_OPTIONS=-DaddVersion -DtargetDirectory=wrappers %OLD_CONSTRUCT_OPTIONS%
 
-call pax-wrap-jar -g backport-util-concurrent -a backport-util-concurrent -v 3.0 -- "-DimportPackage=sun.misc;resolution:=optional,*"
-call pax-wrap-jar -g commons-collections -a commons-collections -v 3.2
+call pax-wrap-jar -a backport-util-concurrent -v 3.0 -- "-DimportPackage=sun.misc;resolution:=optional,*"
+call pax-wrap-jar -a commons-collections -v 3.2
 
-call pax-wrap-jar -g asm -a asm -v 2.2.3
-call pax-wrap-jar -g aopalliance -a aopalliance -v 1.0
+call pax-wrap-jar -a asm -v 2.2.3
+call pax-wrap-jar -a aopalliance -v 1.0
 call pax-wrap-jar -g javax.servlet -a servlet-api -v 2.5
 call pax-wrap-jar -g javax.servlet -a jsp-api -v 2.0
-call pax-wrap-jar -g junit -a junit -v 3.8.1
+call pax-wrap-jar -a junit
 
 set PAX_CONSTRUCT_OPTIONS=%OLD_CONSTRUCT_OPTIONS%
 
