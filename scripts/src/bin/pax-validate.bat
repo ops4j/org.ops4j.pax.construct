f5c5f65e81d960df4b5403f3ad5b7b9ee96b21ec
Stuart McCulloch <mcculls@gmail.com>
2007-11-25 10:45:01 +0000

    PAXCONSTRUCT-54: missing %'s
    git-svn-id: https://scm.ops4j.org/repos/ops4j/projects/pax/construct@9355 9b982a3c-3ae5-0310-a4bc-d9a3335569bd

--- scripts/src/bin/pax-validate.bat
+++ scripts/src/bin/pax-validate.bat
@@ -8,7 +8,7 @@ set _FIND_="find.exe"
 if exist "%SystemRoot%\system32\find.exe" set _FIND_="%SystemRoot%\system32\find.exe"
 if exist "%SystemRoot%\command\find.exe" set _FIND_="%SystemRoot%\command\find.exe"
 
-call mvn -o -npu -N -f "%_SCRIPTS_%\pax-bootstrap-pom.xml" -DPAX_CONSTRUCT_VERSION=%PAX_CONSTRUCT_VERSION% validate | _FIND_ "ERROR" >NUL
+call mvn -o -npu -N -f "%_SCRIPTS_%\pax-bootstrap-pom.xml" -DPAX_CONSTRUCT_VERSION=%PAX_CONSTRUCT_VERSION% validate | %_FIND_% "ERROR" >NUL
 goto answer%ERRORLEVEL%
 
 :answer1
