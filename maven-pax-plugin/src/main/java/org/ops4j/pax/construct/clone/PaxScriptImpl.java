32f5759ccea406ac083bdcfba3fbcdcbbcdebd4e
Stuart McCulloch <mcculls@gmail.com>
2008-03-03 11:04:28 +0000

    Fix windows argument list
    git-svn-id: https://scm.ops4j.org/repos/ops4j/projects/pax/construct@10853 9b982a3c-3ae5-0310-a4bc-d9a3335569bd

--- maven-pax-plugin/src/main/java/org/ops4j/pax/construct/clone/PaxScriptImpl.java
+++ maven-pax-plugin/src/main/java/org/ops4j/pax/construct/clone/PaxScriptImpl.java
@@ -455,7 +455,7 @@ public class PaxScriptImpl
                 // allow customization
                 if( isBatchFile )
                 {
-                    cmd = StringUtils.replace( cmd, " -- ", " %0 %1 %2 %3 %4 %5 %6 %7 %8 %9 -- " );
+                    cmd = StringUtils.replace( cmd, " -- ", " %1 %2 %3 %4 %5 %6 %7 %8 %9 -- " );
                 }
                 else
                 {
